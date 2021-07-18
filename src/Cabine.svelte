deadcd751f9d92b8a15ac9e6d42cf9161de95725
Samuel Lemos <samuel.afonso2008@hotmail.com>
2021-07-18 12:05:18 -0300

    adicionando o arquivo dos componentes

---
+++ src/Cabine.svelte
@@ -0,0 +1,18 @@
+{#each options as option, index (option)}
+<button class="m5" class:selected="{index===selection}" on:click={dispatch('selected', index)}>{option}
+</button>
+{/each}
+
+<script>
+	export let options
+	export let selection
+	import { createEventDispatcher } from 'svelte'
+
+	const dispatch = createEventDispatcher()
+</script>
+
+<style>
+  .selected {
+    font-weight: bold;
+  }
+</style>
